a97b4a49e51e12a07696d95cc3ba752610ca7d92
vanessa <machado.vanessa32@gmail.com>
2022-09-20 22:12:15 -0300

    Eventos de clique, toggle

---
+++ src/app/components/eventos/eventos.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+  show: boolean = false;
+
+  showMessage(): void {
+    this.show = !this.show; //toggle, isso é js;
+  }
+
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}

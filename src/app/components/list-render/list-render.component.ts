2045c2d94b23126dade10fde05aee9464f3943b9
vanessa <machado.vanessa32@gmail.com>
2022-09-21 21:00:45 -0300

    Add metodos

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    {name: 'Turca', type: 'Dog'},
-    {name: 'Tom', type: 'Cat'},
-    {name: 'Frida', type: 'Dog'},
-    {name: 'Bob', type: 'Horse'},
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 4 },
+    { name: 'Tom', type: 'Cat', age: 10 },
+    { name: 'Frida', type: 'Dog', age: 5 },
+    { name: 'Bob', type: 'Horse', age: 10 },
   ];
 
-  constructor() {}
+  animal: Animal = {
+    name: 'Teste',
+    type: 'Alguma Coisa',
+    age: 10,
+  }
 
-  ngOnInit(): void {
+  animalDetails = '';
+
+  constructor(private listService: ListService) { }
+
+  showAge(animal: Animal) {
+    this.animalDetails = `o pet ${animal.name} tem ${animal.age} anos!`;
+  }
+
+  ngOnInit(): void { }
+
+  removeAnimal(animal: Animal) {
+    console.log('Removendo animal');
+    this.animals = this.listService.remove(this.animals, animal);
   }
 
 }

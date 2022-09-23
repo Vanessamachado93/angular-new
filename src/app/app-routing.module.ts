d7be5bdc2b3bffa348451198018c54892739a147
vanessa <machado.vanessa32@gmail.com>
2022-09-23 12:05:21 -0300

    Add metodos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule , Routes } from "@angular/router";
 
 import { FirstComponentComponent } from "./components/first-component/first-component.component";
 import { ListRenderComponent } from "./components/list-render/list-render.component";
+import { ItemDetailsComponent } from "./components/item-details/item-details.component";
 
 const routes: Routes = [
   { path: '', component: FirstComponentComponent },
-  { path: 'list', component: ListRenderComponent }
+  { path: 'list', component: ListRenderComponent },
+  { path: 'list/:id', component: ItemDetailsComponent}
 ]
 
 @NgModule({
